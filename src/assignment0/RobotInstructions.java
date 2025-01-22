8b5c7b3130b5268ede26c5136d206854fdd6de94
yaelgoldwasser <yaelgoldwasser@localhost>
2025-01-22 11:27:27 -0600

    Robot controller

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,30 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		
+		robot.turnLeft(45);
+		robot.forward(150);
+		robot.turnRight(45);
+		robot.forward(50);
+		robot.turnRight(45);
+		robot.forward(50);
+		robot.turnRight(45);
+		robot.forward(70);
+		robot.turnRight(45);
+		robot.forward(50);
 		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(50);
+		robot.turnRight(45);
+		robot.forward(70);
+		robot.turnRight(45);
+		robot.forward(50);
+		robot.turnRight(45);
+		robot.forward(50);
+		robot.turnRight(45);
+		robot.forward(180);
+		
+		
+		
 	}
 
 }
